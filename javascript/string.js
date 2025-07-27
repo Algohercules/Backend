2e069b88c55d9c842a66326f65006478cbb8e12b
Algohercules <adarshrreso@gmail.com>
2025-07-28 01:44:38 +0530

    ways to create a string

---
+++ javascript/string.js
@@ -0,0 +1,13 @@
+// * string is known as array of characters
+
+
+//? ways to create a string 
+
+
+
+let name = "love";
+
+let firstname  = `adarsh
+is a good boy`;
+
+let anothername = new string("love babbar");
