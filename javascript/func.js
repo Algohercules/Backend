027fa5d4c9e97c323065a5e417dbfa53d31f6229
Algohercules <adarshrreso@gmail.com>
2025-07-28 05:59:33 +0530

    arguments and parameters

--- javascript/func.js
+++ javascript/func.js
@@ -1,13 +1,29 @@
+// ! function declaration
 // function sayMYName(){
 //     console.log("adarsh rai");
 // }
 
-// sayMYName();
+// sayMYName();   //* function calling 
 
-function printCounting(){
-    for(let i =0; i<=10; i++){
-        console.log("hey i am running");
-    }
+
+
+// function printCounting(){
+//     for(let i =0; i<=10; i++){
+//         console.log("hey i am running");
+//     }
+// }
+
+// printCounting();
+
+
+
+function getAverage(){
+    let avg = (num1 + num2) / 2 ;
+    console.log("average is :", avg);
 }
 
-printCounting();
+getAverage();
+
+
+
+
