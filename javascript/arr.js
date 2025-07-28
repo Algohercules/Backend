2a64a32696d8b51b8cf41ec7f39a8dc363f6ea61
Algohercules <adarshrreso@gmail.com>
2025-07-28 06:39:02 +0530

    object

---
+++ javascript/arr.js
@@ -0,0 +1,15 @@
+/*
+* primitive data  types are stored in stack style of memory
+
+! reference datatypes like arrays objects and functions are stored in heap style of memory 
+to access this heap memory we need a pointer(reference) which is in stack memory
+*/
+
+
+// let obj ={
+//     name : "love"
+// };
+
+// console.log(obj);
+
+
