815f6fcfafd3cfbdd586b218b6e678682048f99f
Algohercules <adarshrreso@gmail.com>
2025-07-28 06:57:19 +0530

    operations on array

--- javascript/arr.js
+++ javascript/arr.js
@@ -19,10 +19,35 @@ to access this heap memory we need a pointer(reference) which is in stack memory
 
 //! declaration of arrays 
 
-let arr = [1,2,3,4,5];
-console.log(arr);
+// let arr = [1,2,3,4,5];
+// console.log(arr);
 
-//* using array constructor
+// //* using array constructor
 
-let brr = new Array('love', 1, true)
-console.log(brr);
+// let brr = new Array('love', 1, true)
+// console.log(brr);
+
+//?   operations on arrays
+/*
+?   push insert at end
+?   pop deletes the end 
+?   shift remove first item
+?   unshift add first item
+?   slice new part 
+?   splice content change
+
+
+?   map used to direct every element of an array to a function and then return the resultant value 
+
+?  filter  used to create such a function that filters the elements of an array
+
+
+?  reduce  (accumulator and current)
+
+?  sort (ascending and decending)
+
+?  index of 
+
+?  size of 
+
+*/
