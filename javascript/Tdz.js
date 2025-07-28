a768c8bed242a46d2511c98a6ed74e523f01c912
Algohercules <adarshrreso@gmail.com>
2025-07-28 19:41:54 +0530

    temporal dead zone

---
+++ javascript/Tdz.js
@@ -0,0 +1,36 @@
+//?  temporal dead zone :--
+
+// let and const are block scoped they can only eb accessed in block
+
+
+
+console.log(marks);
+let marks = 100;
+
+//! lie no. 7 to 8  are known as temporal dead zone because hoisting is not posssible in case of let 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
