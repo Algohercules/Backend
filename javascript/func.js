1c57d70f57b3ec63efd0a5a7d1745a851bcb9edf
Algohercules <adarshrreso@gmail.com>
2025-07-28 05:55:25 +0530

    calling example

--- javascript/func.js
+++ javascript/func.js
@@ -1,5 +1,13 @@
-function sayMYName(){
-    console.log("adarsh rai");
+// function sayMYName(){
+//     console.log("adarsh rai");
+// }
+
+// sayMYName();
+
+function printCounting(){
+    for(let i =0; i<=10; i++){
+        console.log("hey i am running");
+    }
 }
 
-sayMYName();
+printCounting();
