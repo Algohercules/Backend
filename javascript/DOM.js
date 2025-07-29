8a2452bdace814124fbdf89eb5a25cbca8d1f379
Algohercules <adarshrreso@gmail.com>
2025-07-30 03:24:35 +0530

    functioning of $0

---
+++ javascript/DOM.js
@@ -0,0 +1,26 @@
+//!  DOM : document object model
+
+// go to DOM.html
+
+//?   document.getelement.id("id_name");  this is used to fetch an element from a document 
+//?   document.getelementsByClassName("class_name")
+//?   document.getelementByTagName('tag');
+
+
+//!  query selector
+
+//?  document.queryselector('#id_name');
+//?  document.queryselector('.classname');
+//?  document.queryselector('tag_name');
+
+
+//* queryselector me class name fetch krtte time agagr multiplew classses hain same name se to sirf pehla hi elememnt return hoga unlike ki jab hum .getelement tag ka use kr rhe the to saare same name wla eappaear ho rhe the 
+
+//?  document.queryselectorAll()  is used to fetch all elements with same name 
+
+
+
+//!  another way to access elements
+
+//? select the element go oto console wimdow and press $0
+// the element will be accessed
