7c182699e426e9cb2d516b927cbed57cc5836444
Algohercules <adarshrreso@gmail.com>
2025-07-30 04:16:53 +0530

    event target and listeners

---
+++ javascript/event_listeners.js
@@ -0,0 +1,11 @@
+//? events : announcement done by browser
+
+//* monitorevents(documment);
+
+
+//! event - target
+//  the element at which event is recieved (clicking on a button so button is target)
+
+//! event - listener
+// what actionn has 6ot be performed for the event target 
+
