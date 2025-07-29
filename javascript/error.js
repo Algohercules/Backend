be62d7a0557a4f83a89aca4afdb5fa7dfd8010ab
Algohercules <adarshrreso@gmail.com>
2025-07-29 20:42:44 +0530

    try and catch block

---
+++ javascript/error.js
@@ -0,0 +1,20 @@
+//?  any event due to which the normal flow of execution is interrupted is calle an error 
+
+//! there are two types of error 
+// compile time error --> they are caught during the time of parsing of the code  (before execution of the code )
+// run time error --> the error detected during the compilation of code
+
+//* try-catch block
+
+try{
+    //wo likho jahan error aa skta hai 
+console.log(x);
+// jahan error hai uske baad ka kuchh bhi execute nahi hoga
+
+}
+
+catch(error){
+    // define karo error ke sathkya krna hai
+    console.log("i am inside catch block")
+
+}
