4c4e7bc4e5a69ee6640868e24ce2cd49af5e160c
Algohercules <adarshrreso@gmail.com>
2025-07-31 15:52:24 +0530

    code 2

--- javascript/reflow.js
+++ javascript/reflow.js
@@ -2,12 +2,30 @@
 
 //  code 1 
 
+const t1 = performance.now()
+
 for(let i = 0; i<= 100; i++){
     let para = document.createElement('p');
         para.textContent = "this is para"  + i;
         document.body.appendChild(para);
 }
 
+
+const t2 = performance.now(); 
+
+
 // code 2;
 
+let mydiv = document.createElement('div');
+for(let i = 0; i<= 100; i++){
+    let para = document.createElement('p');
+        para.textContent = "this is para"  + i;
+        mydiv.appendChild(para);
+}
+
+document.body.appendChild(mydiv);
+
+
+
+// performance.now  =>  used to measure performance  time of the code too be run  
 
