c46c9214a5592daa757232db5c607c4bdc2eff9c
Algohercules <adarshrreso@gmail.com>
2025-07-31 15:46:51 +0530

    performance

--- javascript/event_listeners.js
+++ javascript/event_listeners.js
@@ -10,3 +10,4 @@
 // what actionn has 6ot be performed for the event target 
 
 //?  <event_taregt>.addEventListener(<event_type>, <function->action>)
+
---
+++ javascript/reflow.js
@@ -0,0 +1,13 @@
+//! performance  we are not going to change 
+
+//  code 1 
+
+for(let i = 0; i<= 100; i++){
+    let para = document.createElement('p');
+        para.textContent = "this is para"  + i;
+        document.body.appendChild(para);
+}
+
+// code 2;
+
+
