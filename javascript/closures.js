996909128fa1bdcdb250a821144724efc49cc286
Algohercules <adarshrreso@gmail.com>
2025-08-03 19:16:49 +0530

    closures  with functions

--- javascript/closures.js
+++ javascript/closures.js
@@ -3,3 +3,12 @@
 //* ek function ko jab uske data ke sath bind kiya jata hai to use hi closures kehte hain
 
 
+function init (){
+
+    var name = "mozilla" ;
+    function displayname(){
+        console.log("name");
+    }
+    displayname();
+}
+init();
