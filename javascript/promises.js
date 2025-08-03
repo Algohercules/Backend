847f476185a1c91d7e98328a49a00c69b7906ed6
Algohercules <adarshrreso@gmail.com>
2025-08-03 10:38:02 +0530

    first promise

--- javascript/promises.js
+++ javascript/promises.js
@@ -3,3 +3,17 @@
 
 // this feature lets asynchronous methods return values like a synvhronous code .  instead of returning value immediately , ther asynvhronous methid returns a promise to supply the value at some point in the future 
 
+let firsrtpromise = new promise((resolve,reject) => {
+    console.log("adarsh op");
+    reject(new error("internal server error"));
+    
+})
+
+
+
+
+
+
+
+
+
